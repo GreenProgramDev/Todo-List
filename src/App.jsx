8d8d2f207ab2c18ae2590ad0a820a91526d35e23
Sdney Lima <lima.rosimao@gmail.com>
2023-07-08 07:57:20 -0300

    chore/styled: update estructure app and style with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import React, { useState } from 'react'
 import './App.css'
+import CreatTask from './components/CreatTask'
+import Done from './components/Done'
+import Header from './components/Header'
+import Task from './components/Task'
+import Footer from './components/footer'
 
 function App() {
 
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskName, taskDate) => {
+    const newTask = {
+      name: taskName,
+      date: taskDate,
+    };
+
+    setTasks([...tasks, newTask]);
+  };
+
+
+
   return (
     <>
-     <h1 className='text-3xl font-bold underline text-center'>Hello World!!!</h1>
+      <div className='flex flex-col w-full h-full' >
+        <Header/>
+        <CreatTask addTask={addTask} />
+        <Done/>
+        <Task tasks={tasks}/>
+        <Footer/>
+      </div>
     </>
   )
 }
